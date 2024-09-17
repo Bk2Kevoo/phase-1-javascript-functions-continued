631078415922460ce83ca3c68c727b02438e9441
kevin fajardo <kevinfajardo1103@gmail.com>
2024-09-17 17:57:14 -0400

    Changes

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ console.log(saturdayFun("bathe my dog"))
 function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
 }
-console.log(mondayWork("work from home"))
-function mondayWork(activity = 'go to the office') {
-    return `This Monday, I will ${activity}.`
-}
-function wrapAdjective(activity = "*") {
-    return function (adjective = "special") {
-        return `You are ${activity}${adjective}${activity}!`
-    }
 
+const mondayWork = (work = "go to the office") => {
+    return `This Monday, I will ${work}.`
 }
+
+function wrapAdjective(special = "*") {
+    return function (adjective = "special") {
+        return `You are ${special}${adjective}${special}!`
+    }
+}
+
